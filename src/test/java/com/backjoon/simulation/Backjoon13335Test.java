9521274dbae002d33cd42d49642951aa31e7e1ac
JUNGHOJONG <hojong1351@naver.com>
2020-11-01 22:26:50 +0900

    백준 - 트럭(테스트 함수 수정 - truckCount 입력변수 제거)

--- src/test/java/com/backjoon/simulation/Backjoon13335Test.java
+++ src/test/java/com/backjoon/simulation/Backjoon13335Test.java
@@ -10,13 +10,12 @@ public class Backjoon13335Test {
         //GIVEN
         int bridgeLength = 2;
         int maximumLoad = 10;
-        int truckCount = 4;
         int[] trucks = {7, 4, 5, 6};
         int expectedResult = 8;
 
         //WHEN
         Backjoon13335 processCommand = new Backjoon13335();
-        int actualResult = processCommand.main(bridgeLength, maximumLoad, truckCount, trucks);
+        int actualResult = processCommand.main(bridgeLength, maximumLoad, trucks);
 
         //THEN
         Assert.assertEquals(expectedResult, actualResult);
@@ -27,13 +26,12 @@ public class Backjoon13335Test {
         //GIVEN
         int bridgeLength = 5;
         int maximumLoad = 10;
-        int truckCount = 1;
         int[] trucks = {8};
         int expectedResult = 6;
 
         //WHEN
         Backjoon13335 processCommand = new Backjoon13335();
-        int actualResult = processCommand.main(bridgeLength, maximumLoad, truckCount, trucks);
+        int actualResult = processCommand.main(bridgeLength, maximumLoad, trucks);
 
         //THEN
         Assert.assertEquals(expectedResult, actualResult);
@@ -44,13 +42,12 @@ public class Backjoon13335Test {
         //GIVEN
         int bridgeLength = 1;
         int maximumLoad = 20;
-        int truckCount = 5;
         int[] trucks = {8, 7, 5, 6, 2};
         int expectedResult = 6;
 
         //WHEN
         Backjoon13335 processCommand = new Backjoon13335();
-        int actualResult = processCommand.main(bridgeLength, maximumLoad, truckCount, trucks);
+        int actualResult = processCommand.main(bridgeLength, maximumLoad, trucks);
 
         //THEN
         Assert.assertEquals(expectedResult, actualResult);
@@ -61,13 +58,12 @@ public class Backjoon13335Test {
         //GIVEN
         int bridgeLength = 4;
         int maximumLoad = 50;
-        int truckCount = 3;
         int[] trucks = {30, 21, 10};
         int expectedResult = 10;
 
         //WHEN
         Backjoon13335 processCommand = new Backjoon13335();
-        int actualResult = processCommand.main(bridgeLength, maximumLoad, truckCount, trucks);
+        int actualResult = processCommand.main(bridgeLength, maximumLoad, trucks);
 
         //THEN
         Assert.assertEquals(expectedResult, actualResult);
@@ -78,13 +74,12 @@ public class Backjoon13335Test {
         //GIVEN
         int bridgeLength = 1;
         int maximumLoad = 10;
-        int truckCount = 1;
         int[] trucks = {5};
         int expectedResult = 2;
 
         //WHEN
         Backjoon13335 processCommand = new Backjoon13335();
-        int actualResult = processCommand.main(bridgeLength, maximumLoad, truckCount, trucks);
+        int actualResult = processCommand.main(bridgeLength, maximumLoad, trucks);
 
         //THEN
         Assert.assertEquals(expectedResult, actualResult);
@@ -95,13 +90,12 @@ public class Backjoon13335Test {
         //GIVEN
         int bridgeLength = 100;
         int maximumLoad = 100;
-        int truckCount = 1;
         int[] trucks = {10};
         int expectedResult = 101;
 
         //WHEN
         Backjoon13335 processCommand = new Backjoon13335();
-        int actualResult = processCommand.main(bridgeLength, maximumLoad, truckCount, trucks);
+        int actualResult = processCommand.main(bridgeLength, maximumLoad, trucks);
 
         //THEN
         Assert.assertEquals(expectedResult, actualResult);
@@ -112,13 +106,12 @@ public class Backjoon13335Test {
         //GIVEN
         int bridgeLength = 100;
         int maximumLoad = 100;
-        int truckCount = 10;
         int[] trucks = {10, 10, 10, 10, 10, 10, 10, 10, 10, 10};
         int expectedResult = 110;
 
         //WHEN
         Backjoon13335 processCommand = new Backjoon13335();
-        int actualResult = processCommand.main(bridgeLength, maximumLoad, truckCount, trucks);
+        int actualResult = processCommand.main(bridgeLength, maximumLoad, trucks);
 
         //THEN
         Assert.assertEquals(expectedResult, actualResult);
