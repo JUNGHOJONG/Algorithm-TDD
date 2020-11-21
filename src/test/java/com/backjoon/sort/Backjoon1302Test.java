f49a66f406f84aa59e3df1413cfcc001c19c9d11
JUNGHOJONG <hojong1351@naver.com>
2020-11-21 22:13:38 +0900

    백준 - 베스트셀러(테스트 클래스 구현 - 테스트케이스 5개)

---
+++ src/test/java/com/backjoon/sort/Backjoon1302Test.java
@@ -0,0 +1,72 @@
+package com.backjoon.sort;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Backjoon1302Test {
+
+    @Test
+    public void testCase1(){
+        //GIVEN
+        String[] books = {"top", "top", "top", "top", "top", "kimtop"};
+        String expectedResult = "top";
+
+        //WHEN
+        String actualResult = Backjoon1302.mains(books);
+
+        //THEN
+        Assert.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void testCase2(){
+        //GIVEN
+        String[] books = {"top"};
+        String expectedResult = "top";
+
+        //WHEN
+        String actualResult = Backjoon1302.mains(books);
+
+        //THEN
+        Assert.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void testCase3(){
+        //GIVEN
+        String[] books = {"kim", "kim", "stop", "top", "top", "kimtop"};
+        String expectedResult = "kim";
+
+        //WHEN
+        String actualResult = Backjoon1302.mains(books);
+
+        //THEN
+        Assert.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void testCase4(){
+        //GIVEN
+        String[] books = {"top", "kim", "kimss", "total", "ace", "gee"};
+        String expectedResult = "ace";
+
+        //WHEN
+        String actualResult = Backjoon1302.mains(books);
+
+        //THEN
+        Assert.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void testCase5(){
+        //GIVEN
+        String[] books = {"ace", "aceee", "ceee"};
+        String expectedResult = "ace";
+
+        //WHEN
+        String actualResult = Backjoon1302.mains(books);
+
+        //THEN
+        Assert.assertEquals(expectedResult, actualResult);
+    }
+}
