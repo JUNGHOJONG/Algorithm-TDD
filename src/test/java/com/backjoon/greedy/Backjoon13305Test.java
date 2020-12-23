ca3e881578eb1895e06a2a90d55c3af349c0218e
JUNGHOJONG <hojong1351@naver.com>
2020-12-23 22:55:56 +0900

    백준 - 주유소(테스트 클래스 구현 - 테스트케이스 5개)

---
+++ src/test/java/com/backjoon/greedy/Backjoon13305Test.java
@@ -0,0 +1,80 @@
+package com.backjoon.greedy;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class Backjoon13305Test {
+    private Backjoon13305 processCommand;
+
+    @Before
+    public void prepareTest() {
+        processCommand = new Backjoon13305();
+    }
+
+    @Test
+    public void testCase1() {
+        //GIVEN
+        int[] distance = {2, 3, 1};
+        int[] gasStation = {5, 2, 4, 1};
+        int expectedResult = 18;
+
+        //WHEN
+        int actualResult = processCommand.main(distance, gasStation);
+
+        Assert.assertThat(actualResult, CoreMatchers.is(expectedResult));
+    }
+
+    @Test
+    public void testCase2() {
+        //GIVEN
+        int[] distance = {3};
+        int[] gasStation = {4, 3};
+        int expectedResult = 12;
+
+        //WHEN
+        int actualResult = processCommand.main(distance, gasStation);
+
+        Assert.assertThat(actualResult, CoreMatchers.is(expectedResult));
+    }
+
+    @Test
+    public void testCase3() {
+        //GIVEN
+        int[] distance = {5, 2};
+        int[] gasStation = {1, 4, 3};
+        int expectedResult = 7;
+
+        //WHEN
+        int actualResult = processCommand.main(distance, gasStation);
+
+        Assert.assertThat(actualResult, CoreMatchers.is(expectedResult));
+    }
+
+    @Test
+    public void testCase4() {
+        //GIVEN
+        int[] distance = {8, 3, 5, 4, 1, 7};
+        int[] gasStation = {4, 6, 7, 2, 4, 1, 3};
+        int expectedResult = 81;
+
+        //WHEN
+        int actualResult = processCommand.main(distance, gasStation);
+
+        Assert.assertThat(actualResult, CoreMatchers.is(expectedResult));
+    }
+
+    @Test
+    public void testCase5() {
+        //GIVEN
+        int[] distance = {4, 6};
+        int[] gasStation = {5, 3, 2};
+        int expectedResult = 38;
+
+        //WHEN
+        int actualResult = processCommand.main(distance, gasStation);
+
+        Assert.assertThat(actualResult, CoreMatchers.is(expectedResult));
+    }
+}
