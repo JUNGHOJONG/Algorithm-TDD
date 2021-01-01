d625506f81abbbb32710f8c678153a80dc03c772
JUNGHOJONG <hojong1351@naver.com>
2021-01-01 23:41:20 +0900

    백준 - 이친수(테스트 클래스 구현 - 테스트케이스 5개)

---
+++ src/test/java/com/backjoon/dp/Backjoon2193Test.java
@@ -0,0 +1,70 @@
+package com.backjoon.dp;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class Backjoon2193Test {
+    private Backjoon2193 backjoon2193;
+
+    @Before
+    public void prepareTest() {
+        backjoon2193 = new Backjoon2193();
+    }
+
+    @Test
+    public void testCase1() {
+        int pinaryNumberOfDigits = 1;
+
+        int expectedResult = 1;
+
+        int actualResult = backjoon2193.main(pinaryNumberOfDigits);
+
+        Assert.assertThat(actualResult, CoreMatchers.is(expectedResult));
+    }
+
+    @Test
+    public void testCase2() {
+        int pinaryNumberOfDigits = 2;
+
+        int expectedResult = 1;
+
+        int actualResult = backjoon2193.main(pinaryNumberOfDigits);
+
+        Assert.assertThat(actualResult, CoreMatchers.is(expectedResult));
+    }
+
+    @Test
+    public void testCase3() {
+        int pinaryNumberOfDigits = 3;
+
+        int expectedResult = 2;
+
+        int actualResult = backjoon2193.main(pinaryNumberOfDigits);
+
+        Assert.assertThat(actualResult, CoreMatchers.is(expectedResult));
+    }
+
+    @Test
+    public void testCase4() {
+        int pinaryNumberOfDigits = 10;
+
+        int expectedResult = 55;
+
+        int actualResult = backjoon2193.main(pinaryNumberOfDigits);
+
+        Assert.assertThat(actualResult, CoreMatchers.is(expectedResult));
+    }
+
+    @Test
+    public void testCase5() {
+        int pinaryNumberOfDigits = 15;
+
+        int expectedResult = 610;
+
+        int actualResult = backjoon2193.main(pinaryNumberOfDigits);
+
+        Assert.assertThat(actualResult, CoreMatchers.is(expectedResult));
+    }
+}
